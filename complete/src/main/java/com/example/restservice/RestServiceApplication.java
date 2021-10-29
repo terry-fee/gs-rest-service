c9d196e686b15467fd107f2332f019ed426a62da
terry-fee <86654024+terry-fee@users.noreply.github.com>
2021-10-29 16:21:26 +0800

    Update RestServiceApplication.java

--- complete/src/main/java/com/example/restservice/RestServiceApplication.java
+++ complete/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestServiceApplication {
 
+    // Some comments here
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
     }
